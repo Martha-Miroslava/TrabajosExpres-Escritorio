a847ff2abae0daef9f609f3c42f8f8e0c8e0ce86
Martha <martha_15_7@outlook.com>
2021-04-03 13:02:23 -0600

    Agregar archivos de proyecto.

---
+++ TrabajosExpres/AccountEdition.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+
+namespace TrabajosExpres
+{
+    /// <summary>
+    /// Lógica de interacción para AccountEdit.xaml
+    /// </summary>
+    public partial class AccountEdition : Window
+    {
+        public AccountEdition()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TrabajosExpres/AccountRecover.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Windows;
+
+namespace TrabajosExpres
+{
+    /// <summary>
+    /// Lógica de interacción para RecoverAccount.xaml
+    /// </summary>
+    public partial class AccountRecover : Window
+    {
+        public AccountRecover()
+        {
+            InitializeComponent();
+        }
+    }
+}

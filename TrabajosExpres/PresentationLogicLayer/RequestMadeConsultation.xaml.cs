1ab3d193bec6df261439a7049cad3ff447328438
Alemin <ale_200200@hotmail.com>
2021-04-26 22:08:01 -0500

    División por capas
    División por capas y GUI faltantes

---
+++ TrabajosExpres/PresentationLogicLayer/RequestMadeConsultation.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TrabajosExpres.PresentationLogicLayer
+{
+    /// <summary>
+    /// Lógica de interacción para RequestMadeConsultation.xaml
+    /// </summary>
+    public partial class RequestMadeConsultation : Window
+    {
+        public RequestMadeConsultation()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButtonClicked(object sender, RoutedEventArgs routedEventArgs)
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show("¿Seguro que desea cancelar esta solicitud?",
+                "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult == MessageBoxResult.Yes)
+            {
+                
+            }
+        }
+    }
+}

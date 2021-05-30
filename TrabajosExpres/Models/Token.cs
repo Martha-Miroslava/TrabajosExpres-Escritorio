cac4ae4bd19d384a8e3b0215c77aefb7b1697673
Martha <martha_15_7@outlook.com>
2021-05-30 12:13:05 -0500

    Funcionalidad Login
    Creación de la funcionalidad del Login y la conexion con la API

---
+++ TrabajosExpres/Models/Token.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabajosExpres.Models
+{
+    public class Token
+    {
+        public string token { get; set; }
+        public int memberATEType { get; set; }
+        public int idMemberATE { get; set; }
+    }
+}
--- TrabajosExpres/Validators/LoginValidator.cs
+++ TrabajosExpres/Validators/LoginValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using System.Text.RegularExpressions;
 using TrabajosExpres.Models;
 
 namespace TrabajosExpres.Validators
@@ -10,12 +11,29 @@ namespace TrabajosExpres.Validators
             RuleFor(login => login.username).NotEmpty().WithState(login => "TextBoxUsername")
                     .MaximumLength(20).WithState(login => "TextBoxUsername")
                     .MaximumLength(8).WithState(login => "TextBoxUsername")
-                    .Matches(@"^([A-Z0-9]{1}[a-z0-9]+[\\s0-9]*)+\$").WithState(user => "TextBoxUsername");
+                    .Matches("^[A-Za-z0-9]+$").WithState(user => "TextBoxUsername");
 
             RuleFor(login => login.password).NotEmpty().WithState(login => "PasswordBoxPassword")
                     .MaximumLength(15).WithState(login => "PasswordBoxPassword")
                     .MinimumLength(8).WithState(login => "PasswordBoxPassword")
-                    .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\$@\$!%*?&#])[A-Za-z\\d\$@\$!%*?&#]{8,15}").WithState(account => "PasswordBoxPassword");
+                    .Must(BeValidPassword).WithState(account => "PasswordBoxPassword");
+        }
+
+        public static bool BeValidPassword(string password)
+        {
+            bool isValidPassword = false;
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMiniMaxChars = new Regex(@".{8,15}");
+            var hasLowerChar = new Regex(@"[a-z]+");
+            var hasSymbols = new Regex(@"[@#_]");
+            if (hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) &&
+                hasMiniMaxChars.IsMatch(password) && hasLowerChar.IsMatch(password) && hasSymbols.IsMatch(password))
+            {
+                isValidPassword = true;
+            }
+            return isValidPassword;
         }
     }
+ 
 }

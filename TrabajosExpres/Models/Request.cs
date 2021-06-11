a089a8aa6c2c60942bb748c1157d21b058bec198
Martha <martha_15_7@outlook.com>
2021-06-11 01:14:02 -0500

    Realización de la funcionalidad solicitar servicio
    Realización de la funcionalidad solicitar servicio

--- TrabajosExpres/Models/Request.cs
+++ TrabajosExpres/Models/Request.cs
@@ -6,8 +6,8 @@ namespace TrabajosExpres.Models
     {
         public int idRequest { get; set; }
         public string address { get; set; }
-        public DateTime date { get; set; }
-        public DateTime time { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
         public string trouble { get; set; }
         public int idMemberATE { get; set; }
         public int idService { get; set; }

a1af6942a72292b17111f853d350b94079edf0b3
Martha <martha_15_7@outlook.com>
2021-06-14 12:05:58 -0500

    Realización de la funcionalidad consultar comentarios de los clientes
    Realización de la funcionalidad consultar comentarios de los clientes

---
+++ TrabajosExpres/Models/RatingReceived.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TrabajosExpres.Models
+{
+    public class RatingReceived
+    {
+        public int idRating { get; set; }
+        public string comment { get; set; }
+        public int rating { get; set; }
+        public int idRequest { get; set; }
+        public string isClient { get; set; }
+    }
+}
